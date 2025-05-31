13cce8023c69ea439c98919d32183fce1205d1df
Petar Štika <stika.petar@gmail.com>
2025-05-31 16:16:26 +0200

    Add low res patch and high res patch

--- augment.py
+++ augment.py
@@ -7,11 +7,17 @@ import torch
 import torchvision
 import torchvision.transforms as T
 import torchvision.transforms.functional as TF
+from models.utils import is_img_file
 
 # Taken from: https://github.com/wangzhesun/super_resolution
 # In paper they use hr_patch_size
-def random_crop(lr_img, hr_img, lr_patch_size=48, scale_factor=2):
-    hr_patch_size = lr_patch_size * scale_factor
+def random_crop(lr_img, hr_img, lr_patch_size=None, hr_patch_size=None, scale_factor=2):
+    assert (lr_patch_size is not None) ^ (hr_patch_size is not None), "Exactly one of lr_patch_size or hr_patch_size must be provided."
+    if hr_patch_size is not None:
+        lr_patch_size = hr_patch_size // scale_factor
+    else:
+        hr_patch_size = lr_patch_size * scale_factor
+
     h, w = lr_img.shape[1:3]
 
     lr_width = random.randint(0, w - lr_patch_size)
@@ -33,12 +39,18 @@ def random_rotate(lr_img, hr_img):
     random_val = random.randint(0, 3)
     return torch.rot90(lr_img, random_val, (1, 2)), torch.rot90(hr_img, random_val, (1, 2))
 
-def augment_img(lr_img_file, hr_img_file, output_dir, n_augmentations, lr_patch_size, scale_factor):
+def augment_img(lr_img_file, hr_img_file, output_dir, n_augmentations, scale_factor=2, lr_patch_size=None, hr_patch_size=None):
     lr_img = TF.to_tensor(Image.open(lr_img_file).convert("RGB"))
     hr_img = TF.to_tensor(Image.open(hr_img_file).convert("RGB"))
 
     for i in range(n_augmentations):
-        lr_cropped_img, hr_cropped_img = random_crop(lr_img, hr_img, lr_patch_size=lr_patch_size, scale_factor=scale_factor)
+        lr_cropped_img, hr_cropped_img = random_crop(
+            lr_img,
+            hr_img, 
+            lr_patch_size=lr_patch_size,
+            hr_patch_size=hr_patch_size,
+            scale_factor=scale_factor
+        )
         if random.random() < 0.5:
             lr_cropped_img, hr_cropped_img = random_flip(lr_cropped_img, hr_cropped_img)
         if random.random() < 0.5:
@@ -53,30 +65,37 @@ def augment_img(lr_img_file, hr_img_file, output_dir, n_augmentations, lr_patch_
         TF.to_pil_image(lr_cropped_img).save(lr_out)
         TF.to_pil_image(hr_cropped_img).save(hr_out)
             
-def augment_dir(data_dir, output_dir, n_augmentations=5, lr_patch_size=48, scale_factor=2):
-    lr_files = sorted([f for f in os.listdir(f"{data_dir}/X{scale_factor}/LR") if f.endswith((".png", ".jpg"))])
-    hr_files = sorted([f for f in os.listdir(f"{data_dir}/X{scale_factor}/HR") if f.endswith((".png", ".jpg"))])
-    os.makedirs(f"{output_dir}/X{scale_factor}/LR", exist_ok=True)
-    os.makedirs(f"{output_dir}/X{scale_factor}/HR", exist_ok=True)
+def augment_dir(data_dir, output_dir, n_augmentations=5, lr_patch_size=None, hr_patch_size=None, scale_factor=2):
+    lr_dir = os.path.join(data_dir, f"X{scale_factor}/LR")
+    hr_dir = os.path.join(data_dir, f"X{scale_factor}/HR")
+    lr_img_files = sorted([os.path.join(lr_dir, f) for f in os.listdir(lr_dir) if is_img_file(os.path.join(lr_dir, f))])
+    hr_img_files = sorted([os.path.join(hr_dir, f) for f in os.listdir(hr_dir) if is_img_file(os.path.join(hr_dir, f))])
     
-    for i in range(len(lr_files)):
+    os.makedirs(os.path.join(output_dir, f"X{scale_factor}/LR"), exist_ok=True)
+    os.makedirs(os.path.join(output_dir, f"X{scale_factor}/HR"), exist_ok=True)
+    
+    for i, (lr_img_file, hr_img_file) in enumerate(zip(lr_img_files, hr_img_files)):
         print(f"Augmenting image {i + 1}...")
         augment_img(
-            os.path.join(f"{data_dir}/X{scale_factor}/LR", lr_files[i]),
-            os.path.join(f"{data_dir}/X{scale_factor}/HR", hr_files[i]),
-            output_dir,
+            lr_img_file=lr_img_file,
+            hr_img_file=hr_img_file,
+            output_dir=output_dir,
             n_augmentations=n_augmentations, 
-            lr_patch_size=lr_patch_size, 
+            lr_patch_size=lr_patch_size,
+            hr_patch_size=hr_patch_size,
             scale_factor=scale_factor
         )
 
 if __name__ == "__main__":
-    for dataset in ["Urban100", "Set5", "Set14", "BSD100", "DIV2K-val", "DIV2K-train"]:
+    # for dataset in ["Urban100", "Set5", "Set14", "BSD100", "DIV2K-val", "DIV2K-train"]:
+    for dataset in ["Set5"]:
         for scale_factor in [2, 3, 4]:
             random.seed(0)
             augment_dir(
                 data_dir=f"./data/{dataset}",
-                output_dir=f"./data-augmented/{dataset}",
-                n_augmentations=10,
-                scale_factor=scale_factor
+                output_dir=f"./data-augmented4/{dataset}",
+                n_augmentations=20,
+                scale_factor=scale_factor,
+                hr_patch_size=192
+                # lr_patch_size=48
             )
